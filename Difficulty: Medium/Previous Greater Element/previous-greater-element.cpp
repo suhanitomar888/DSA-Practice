0f1a5f6a3650a87e0a1d865e0e3ee5125cd2df85
Suhani Tomar <144531493+suhanitomar888@users.noreply.github.com>
2026-03-12 01:12:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Previous Greater Element/previous-greater-element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    vector<int> preGreaterEle(vector<int>& arr) {
+        int n=arr.size();
+        stack<int> st;
+        vector<int> pse(n);
+        for(int i=0;i<n;i++){
+            while(!st.empty() && st.top()<=arr[i]){
+                st.pop();
+            }
+            if(st.empty())pse[i]=-1;
+            else pse[i]=st.top();
+            st.push(arr[i]);
+        }
+        return pse;
+    }
+};

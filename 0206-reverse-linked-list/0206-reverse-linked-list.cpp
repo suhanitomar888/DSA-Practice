224a2ff9e40605b2adae0f7513b388a50a67ec5b
Suhani Tomar <144531493+suhanitomar888@users.noreply.github.com>
2026-04-17 02:14:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head){
+        ListNode*prev=nullptr;
+        ListNode*curr=head;
+        while(curr!=nullptr){
+            ListNode*nn=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nn;
+        }
+        return prev;
+    }
+};

59e5cf783cbb4b3ea6bed148048915bb1610fec8
Suhani Tomar <144531493+suhanitomar888@users.noreply.github.com>
2026-04-22 17:56:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void helper(TreeNode* root, vector<int>& ans){
+        if(!root)return;
+        ans.push_back(root->val);
+        helper(root->left,ans);
+        helper(root->right,ans);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        helper(root,ans);
+        return ans;
+    }
+};

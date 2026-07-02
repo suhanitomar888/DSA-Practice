db4837b7dfb77ead6a07860a4995e0e3060921ac
Suhani Tomar <144531493+suhanitomar888@users.noreply.github.com>
2026-07-03 00:35:49 +0530

    Time: 0 ms (100%), Space: 12.6 MB (13.69%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums){
-        for(int i=1;i<nums.size();i++){
-            nums[i]+=nums[i-1];         }
-        return nums;
+        vector<int>nn;
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            nn.push_back(sum);
+        }
+        return nn;
     }
 };

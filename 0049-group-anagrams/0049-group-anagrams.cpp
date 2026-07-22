a7b5ec774f7b206d7f99ed63779096b72726b043
Suhani Tomar <144531493+suhanitomar888@users.noreply.github.com>
2026-07-23 01:30:41 +0530

    Time: 19 ms (45.02%), Space: 26.1 MB (49.33%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
+        // Map:
+        // Key   -> Sorted version of the string
+        // Value -> All strings having the same sorted form
         unordered_map<string, vector<string>> mp;
 
-        for (string s : strs){
-            string key = s;
+        // Traverse every string in the input
+        for (string word : strs) {
+
+            // Make a copy so that the original word remains unchanged
+            string key = word;
+
+            // Sort the copy to create a unique key
+            // Example:
+            // "eat" -> "aet"
+            // "tea" -> "aet"
             sort(key.begin(), key.end());
-            mp[key].push_back(s);
+
+            // Store the original word in the corresponding group
+            mp[key].push_back(word);
         }
 
+        // This will store the final grouped anagrams
         vector<vector<string>> ans;
 
-        for(auto it : mp){
+        // Traverse the map
+        for (auto it : mp) {
+
+            // Add each group of anagrams to the answer
             ans.push_back(it.second);
         }
 
+        // Return all groups
         return ans;
     }
 };

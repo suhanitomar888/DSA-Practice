566ec7cf016e3d95d4cee7e51ad7ce7fd8240a6e
Suhani Tomar <144531493+suhanitomar888@users.noreply.github.com>
2026-07-22 20:23:39 +0530

    Time: 0 ms (100%), Space: 13.9 MB (99.56%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> dp;
+        for(int i : nums){
+            auto it = lower_bound(dp.begin(), dp.end(), i);
+            if(it == dp.end()){
+                dp.push_back(i);
+            }
+            else{
+                *it = i;
+            }
+        }
+        return dp.size();
+    }
+};

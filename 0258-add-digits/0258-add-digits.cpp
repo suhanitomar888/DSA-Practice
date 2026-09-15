662c39dca32c56c8349b57c1edd89de1de161bd5
Suhani Tomar <144531493+suhanitomar888@users.noreply.github.com>
2026-09-15 16:56:21 +0530

    Time: 0 ms (100%), Space: 8.6 MB (79.34%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    int helper(int num){
-        vector<int>newNum;
-        while(num>0){
-            int ld=num%10;
-            newNum.push_back(ld);
-            num=num/10;
+    int helper(int num) {
+        int sum = 0;
+
+        while (num > 0) {
+            int ld = num % 10;
+            sum += ld;
+            num = num / 10;
         }
-        int sum=0;
-        for(int i=0;i<newNum.size();i++){
-            sum+=newNum[i];
-        }
-        if(sum<=9)return sum;
-        else return helper(sum);
+
+        if (sum <= 9)
+            return sum;
+        else
+            return helper(sum);
     }
-    int addDigits(int num){
+
+    int addDigits(int num) {
         return helper(num);
     }
 };

98ac33f53fe7108cf6a5a6587f2bb51f7eb79ddf
Suhani Tomar <144531493+suhanitomar888@users.noreply.github.com>
2026-09-16 03:37:31 +0530

    Time: 0 ms (100%), Space: 9.8 MB (77%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    bool isAlphaNum(char ch){
-        if((ch>='0' && ch<='9') || (tolower(ch)>='a' && tolower(ch)<='z')) return true;
-        return false;
-    }
     bool isPalindrome(string s) {
-        int st=0;
-        int end=s.length()-1;
-        while(st < end){
-            if(!isAlphaNum(s[st])){
-                st++;
-                continue;
-            }
-            if(!isAlphaNum(s[end])){
-                end--;
-                continue;
-            }
-            if( tolower( s[st] )  != tolower( s[end] ) ){
+        int size = s.length();
+        int i = 0;
+        int j = size - 1;
+        while(i < j){
+            if(!isalnum(s[i]))
+                i++;
+            else if(!isalnum(s[j])){
+                j--;
+            }else if(tolower(s[i])!=tolower(s[j])){
                 return false;
+            }else{
+                i++;
+                j--;
             }
-            st++;
-            end--;
         }
         return true;
     }

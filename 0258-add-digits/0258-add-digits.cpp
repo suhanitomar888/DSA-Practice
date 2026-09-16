3e8a22c5f5c9dcd97c04d82863894afc5471f762
Suhani Tomar <144531493+suhanitomar888@users.noreply.github.com>
2026-09-16 12:26:04 +0530

    Time: 0 ms (100%), Space: 8.6 MB (45.43%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    int helper(int num) {
+    int helper(int n){
         int sum=0;
-        while(num>0){
-            int ld=num%10;
+        while(n>0){
+            int ld=n%10;
             sum+=ld;
-            num=num/10;
+            n=n/10;
         }
         if(sum<=9)return sum;
         else return helper(sum);
     }
-
-    int addDigits(int num) {
-        return helper(num);
+    int addDigits(int n){
+        return helper(n);
     }
 };
